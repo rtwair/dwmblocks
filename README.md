898498816832ec2f1190e406aca48a5889ace2d2
rtwair@gmail.com <riyad@lilyandlou.com>
2020-09-23 15:38:30 +0800

    Removed signals to fix nasty bug

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # dwmblocks
 Modular status bar for dwm written in c.
 
-# Riyad's DWM Build
-  * Includes some specific patches to accept signals for updates (hack for event based triggers)
-    - Also includes lukes `statuscommands` patch
+# Riyad's DWMBlocks Build
+  * Removed signaling after finding nasty bug (for now I'll add it again in another branch for testing in the near future)
   * Patches are located in the `Patches` directory
 
 # modifying blocks
@@ -11,5 +10,6 @@ The statusbar is made from text output from commandline programs.
 Blocks are added and removed by editing the blocks.h header file.
 By default the blocks.h header file is created the first time you run make which copies the default config from blocks.def.h.
 This is so you can edit your status bar commands and they will not get overwritten in a future update.
+Checkout my statusbar repo for some statuses that I've built.
 
 
--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,35 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	/*{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},*/
+	/* {"",	"music",	0,	11},*/
+	/*{"",	"pacpackages",	0,	8},*/
+	/*{"",	"news",		0,	6},*/
+	/* {"",	"crypto",	0,	13}, */
+	/* {"",	"price bat \"Basic Attention Token\" 🦁",	0,	20}, */
+	/* {"",	"price btc Bitcoin 💰",				0,	21}, */
+	/* {"",	"price lbc \"LBRY Token\" 📚",			0,	22}, */
+	{"",	"memory.sh",	10,	14}, 
+	{"",	"cputemp.sh",	10,	18}, 
+	{"",	"volume.sh",	0,	10},
+	{"",	"battery.sh",	5,	3},
+	{"",	"clock.sh",	60,	1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static char delim = ' | ';
+
+/*
+ * 
+	{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
+	
+
+	{"", "~/bin/statusbar/volume",						0,		10},
+
+	{"", "~/bin/statusbar/battery",						5,		0},
+
+	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
+
+	{"", "~/bin/statusbar/clock",						5,		0},
+
+*/ 
